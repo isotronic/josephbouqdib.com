46531b01cdcbd34743f34fb51f194a87a9a4ce4c
Joseph Bouqdib <joseph@bouqdib.com>
2024-06-24 17:04:43 +0100

    headless ui and hero icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { HandRaisedIcon } from "@heroicons/react/24/solid";
 function App() {
   return (
-    <div className="flex min-h-screen flex-row items-center justify-center bg-slate-900 leading-relaxed text-slate-300">
+    <div className="flex min-h-screen flex-row items-center justify-center gap-3 bg-slate-900 leading-relaxed text-slate-300">
       <h1 className="text-3xl">Hi there!</h1>
+      <HandRaisedIcon className="size-8" />
     </div>
   );
 }
