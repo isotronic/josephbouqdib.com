ddda740d099916b94e5ebc9ee76e5c9d6561ae4a
Joseph Bouqdib <joseph@bouqdib.com>
2024-06-29 20:11:00 +0200

    build: clean up configuration (#14)
    * build: clean up configuration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,13 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Joseph Bouqdib`,
-    siteUrl: `https://bouqdib.com`,
+    siteUrl: `https://joseph.bouqdib.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap"],
+  plugins: ["gatsby-plugin-postcss"],
 };
 
 export default config;
